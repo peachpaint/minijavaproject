829cc3823d587c4cf1b1a14fa160c3fd6522087b
peachpaint <cheaco2269@gmail.com>
2025-08-26 16:42:23 +0900

    kiosk1

---
+++ src/kiosk/Kiosk.java
@@ -0,0 +1,32 @@
+package kiosk;
+
+import java.util.Scanner;
+
+public class Kiosk {
+
+	public static void main(String[] args) {
+		String msg = """
+				[ MAIN MENU ]
+				1.커피 Coffe
+				2.라떼 Latte
+				3.스무디 Smoothie
+				4.티 Tea
+				5.주문취소
+				""";
+		String msg1 = """
+				[ 커피 CoffeMenu ]
+				""";
+		String msg2 = """
+				[ 라떼 LatteMenu ]
+				""";
+		String msg3 = """
+				[ 스무디 SmoothieMenu ]
+				""";
+		String msg4 = """
+				[ 티 TeaMenu ]
+				""";
+		Scanner in = new Scanner(System.in);
+
+	}
+
+}
